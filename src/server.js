5f98c6c9ddda6e20cf09ec5fb11b37b3f52beece
Husain Trivedi <HUSAINTD52@GMAIL.COM>
2026-03-30 19:10:29 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,23 +1,7 @@
 const app = require('./app');
 
-// hardcoded port (code smell)
-const PORT = 3000;
-
-// duplicate logic (duplication issue)
-const PORT2 = 3000;
-
-// useless function
-function startServer() {
-    console.log("Starting server...");
-}
-
-startServer();
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
-
-// duplicate server start (issue)
-app.listen(PORT2, () => {
-    console.log(`Server running again on port ${PORT2}`);
 });
